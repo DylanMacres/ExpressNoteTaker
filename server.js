1013c5636692ac3472abdd0963c77f233e2445a4
DylanMacres <Dylan.Macres@du.edu>
2022-09-28 15:21:52 -0600

    added api data line for the notes

--- server.js
+++ server.js
@@ -22,20 +22,24 @@ app.use('/', routes );
 app.use(express.static('public'));
 
 
-
+//if the url is in /notes it will show the notes html page
 app.get('/notes', (req, res) => 
     res.sendFile(path.join(__dirname, 'public/notes.html'))
 );
 
+//gathers the data from the notes 
+app.get('/api/notes', (req,res) => {
+    res.json(notes)
+});
 
 
 
-
+//added a catch all if the code gets this far without being executed
 app.get('*', (req, res) => 
 res.sendFile(path.join(__dirname,'/public/index.html'))
 );
 
-
+//link to the http for the server
 app.listen(PORT, () => 
 console.log(`App listening at http://localhost:${PORT}`)
 );
