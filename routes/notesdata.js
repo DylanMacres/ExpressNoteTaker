d25daea8a29a5753daef424ed3888ba1eb1bd5b2
DylanMacres <Dylan.Macres@du.edu>
2022-09-29 01:28:57 -0600

    working on making the notes populate

--- routes/notesdata.js
+++ routes/notesdata.js
@@ -1,3 +1,10 @@
+//boiler plate for making the routes accessible
+const express = require('express');
+const { fstat } = require('fs');
+const router = express.router();
+const path = require('path');
+let db = require("./db/db.json");
+
 
 
 
@@ -10,4 +17,23 @@ app.get('/api/notes', (req,res) => {
 //data from the specific id notation
 app.get('/api/notes/:id', (req, res) => {
     res.json(notes)
+});
+
+app.post("/api/notes", (req, res) => {
+    db = JSON.parse(db);
+    res.json(db)
+    let newNote = {
+        id: uniqID(),
+        title: req.body.title,
+        text: req.body.text,
+    };
+    db.push(newNote);
+    fs.WriteFileSync(db,JSON.stringify(db));
+    res.json(db);
+});
+
+app.delete("/api/nots/:id", (req,res) => {
+    const deleted =req.params.id;
+    const notes = JSON.parse(fs.readFileSync("./db/db.json"));
+
 })
